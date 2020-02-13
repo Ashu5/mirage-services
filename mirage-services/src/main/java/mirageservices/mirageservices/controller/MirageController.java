ba7f0d21d3cb97f6af1be1b2cd81dfbe83d4f9ed
Ashutosh Kumar Dwivedi <www.ashu5@gmail.com>
2020-02-13 17:45:45 +0530

    Update MirageController.java
    Removed json import

--- mirage-services/src/main/java/mirageservices/mirageservices/controller/MirageController.java
+++ mirage-services/src/main/java/mirageservices/mirageservices/controller/MirageController.java
@@ -2,9 +2,6 @@ package mirageservices.mirageservices.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
